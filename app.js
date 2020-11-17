128df084c269812369598e58a3cb152a8b018b0a
dipscoder <dipeshjaiswal140@gmail.com>
2020-11-17 17:24:05 +0530

    Level-5 - Cookies and Session

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-// * Level-3 ---> Hashing and Salting With bcrypt
-
+// * Level-5 ---> Cookies and Session
+// * Pasport ---> http://www.passportjs.org/
 require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
+const session = require('express-session')
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
+const { Passport } = require('passport');
 
 const app = express()
 
@@ -14,16 +16,31 @@ app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended : true}))
 app.set('view engine','ejs')
 
+app.use(session({
+    secret: "Our little secret. ",      // TODO Put this in an env file afterwards
+    resave: false,
+    saveUninitialized: false
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 mongoose.connect("mongodb://localhost/userDB" , {useNewUrlParser:true , useUnifiedTopology: true})
+mongoose.set('useCreateIndex', true);
 // console.log(md5('123456'));
 
 const userSchema = new mongoose.Schema({
     email : String,
     password : String
 })
+userSchema.plugin(passportLocalMongoose)
 
 const User = new mongoose.model('User',userSchema)
+
+passport.use(User.createStrategy());
+ 
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get('/',(req,res)=>{
     res.render('home')
@@ -36,53 +53,54 @@ app.get('/login',(req,res)=>{
     res.render('login')
 })
 
+app.get('/logout',(req,res)=>{
+    req.logout();
+    res.redirect('/');
+})
+
+app.get('/secrets' , (req,res)=>{
+     if (req.isAuthenticated()) {
+         res.render('secrets')
+     } else {
+         res.redirect('/login')
+     }
+})
+
 app.post('/register',(req,res)=>{
 
-
-
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {        //*---> bcrypt
-        // Store hash in your password DB.
-        const newUser = new User({
-            email : req.body.username,
-            password : hash
-        })
-
-        newUser.save((err) => {
-            if (err) {
-                console.log(err);       
-            } else {
-                res.render('secrets')
-            }
-        })
-    }); 
-
-
-
+    User.register({username : req.body.username} , req.body.password , function(err,user) {
+        if (err) {
+            console.log(err);
+            res.redirect('/')
+        } else {
+            passport.authenticate('local')(req,res,function() {
+                res.redirect('/secrets')
+            })
+        }
+    })
 })
 
 app.post('/login',(req,res) => {
+    // imp Problem - Whenever req.login() gets executed a cookie gets created even if i enter wrong password.Even though it displays unauthorized page i can access /secrets route without being redirected to login page
+    // imp Link - https://www.udemy.com/course/the-complete-web-development-bootcamp/learn/lecture/13559534#questions/13061428
+    // const user = new User({
+    //     username : req.body.username,
+    //     password : req.body.password 
+    // })
 
-    const username = req.body.username
-    const password = req.body.password 
-
-    User.findOne({email : username} , (err,foundUser) => {
-        if (err) {
-            console.log(err);
-        } else {
-            if (foundUser) {
-                
-                bcrypt.compare(password, foundUser.password, function(err, result) {  //*--->bcrypt
-                    // result == true
-                    if (result === true) {
-                        res.render('secrets')
-                    } else {
-                        console.log("Wrong credential!");
-                    }
-                });
-
-            } 
-        }
-    })
+    // req.login(user,function(err){
+    //     if (err) {
+    //         console.log(err);
+    //     } else {
+    //         passport.authenticate('local')(req,res,function() {
+    //             res.redirect('/secrets')
+    //         })
+    //     }
+    // })
+    // imp Solution - https://www.udemy.com/course/the-complete-web-development-bootcamp/learn/lecture/13559534#questions/12462002
+    passport.authenticate('local', { successRedirect: '/secrets',
+    failureRedirect: '/login',
+    failureFlash: true })(req,res)
 })
 
 app.listen(3000,(req,res)=>{
